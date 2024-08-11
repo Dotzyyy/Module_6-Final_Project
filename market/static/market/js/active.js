cd619c7ba7e6575646cfb1ce01a5f8435b786f35
David Sexton <davidsexton36@gmail.com>
2024-08-11 17:46:04 +0100

    all products css

--- market/static/market/js/active.js
+++ market/static/market/js/active.js
@@ -3,7 +3,7 @@
 
 document.querySelectorAll(".nav-link").forEach((link) => {  // for each link the group 
     if (link.href === window.location.href) {   // if the current href matches a link, highlight said link 
-        link.classList.add("nav-active");
+        link.classList.add(".nav-active");
        
     }
     
